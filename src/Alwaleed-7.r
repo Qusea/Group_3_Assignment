1e543884dce7817b5a9911b864bba064632363d9
Alwaleed <alhazmi.alwaleed@outlook.com>
2022-10-12 21:27:04 +0000

    new changes

--- src/Alwaleed-7.r
+++ src/Alwaleed-7.r
@@ -29,10 +29,11 @@ flights_weather <- merge(flights,weather,by=c("origin", "year", "month", "day", 
 # Is there any relationship between temp and air_time?
 flights_weather %>%
   group_by(dest, origin) %>%
-  select(temp, air_time, dest) %>%
-  ggplot(aes(x = air_time, y = temp)) +
-  geom_point() +
-  facet_wrap(vars(dest), scales = "free")
+  select(temp, dep_delay, dest) %>%
+  ggplot(aes(x = dep_delay, y = dest, color = temp)) +
+  geom_point(position = position_jitter(height = 0.25)) +
+  facet_grid(cols = vars(origin))
+
 # Merging the flights_EV and weather
 flights_EV_weather <- merge(flights_EV,weather,by=c("origin", "year", "month", "day", "time_hour", "hour"))
 
@@ -46,6 +47,39 @@ flights_EV_weather <- merge(flights_EV,weather,by=c("origin", "year", "month", "
 
 
 
+
+# Counting the number of flights for every season
+flights_EV %>%
+  group_by(season) %>%
+  summarise(count = n())
+
+# Calculating average departure delay per season for every origin
+flights_EV_weather %>%
+  group_by(season, origin) %>%
+  summarise(mean(dep_delay))
+
+# Calculating average departure delay per season for every origin and destination
+avg_dep_delay <- flights_EV_weather %>%
+  group_by(season, origin, dest) %>%
+  summarise(mean(dep_delay))
+
+# Is there a relationship between departure delay and the season?
+flights_EV_weather <- mutate(flights_EV_weather, season = ifelse(month %in% 9:11, "fall",
+                                                 ifelse(month %in% 3:5, "spring",
+                                                        ifelse(month %in% 6:8, "summer","winter"))))
+
+# slice_flights_EV_weather <- flights_EV_weather %>%
+#  group_by(dest) %>%
+#  unique()
+
+# slice_flights_EV_weather %>%
+#  group_by(dest, origin) %>%
+#  select(season, dep_delay, dest) %>%
+#  ggplot(aes(x = dep_delay, y = dest, color = season)) +
+#  geom_point(position = position_jitter(height = 0.25)) +
+#  facet_grid(cols = vars(origin))
+
+
 # Filtering the flights to take only carrier EV
 flights_EV <- flights %>%
   filter(carrier == "EV")
@@ -57,7 +91,12 @@ flights_EV <- mutate(flights_EV, season = ifelse(month %in% 9:11, "fall",
                                                  ifelse(month %in% 3:5, "spring",
                                                         ifelse(month %in% 6:8, "summer","winter"))))
 
-# Counting the number of flights for every season
-flights_EV %>%
-  group_by(season) %>%
-  summarise(count = n())
+
+
+# Plotting the average departure delay vs destination
+# dest_10 %>%
+#  group_by(dest, origin) %>%
+#  select(season, `mean(dep_delay)`, dest) %>%
+#  ggplot(aes(x = 'mean(dep_delay)', y = dest, color = season)) +
+#  geom_point(position = position_jitter(height = 0.25)) +
+#  facet_grid(cols = vars(origin))
